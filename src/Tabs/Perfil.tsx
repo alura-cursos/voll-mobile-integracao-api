c8243c9b19c24ac32c4442bfde654f8c5836b6de
andreocunha <andreoliveiracunha20@gmail.com>
2023-04-19 15:31:12 -0300

    aula 4.2

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -3,13 +3,15 @@ import { Titulo } from '../componentes/Titulo'
 import { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { pegarDadosPaciente } from '../servicos/PacienteServico'
+import { Paciente } from '../interfaces/Paciente'
+import { Botao } from '../componentes/Botao'
 
-export default function Perfil(){
-  const [dadosPaciente, setDadosPaciente] = useState({})
+export default function Perfil({ navigation } : any){
+  const [dadosPaciente, setDadosPaciente] = useState({} as Paciente)
 
   useEffect(() => {
     async function dadosPaciente(){
-      const pacienteId = await AsyncStorage.getItem('pacientId')
+      const pacienteId = await AsyncStorage.getItem('pacienteId')
       if(!pacienteId) return null
 
       const resultado = await pegarDadosPaciente(pacienteId)
@@ -21,23 +23,36 @@ export default function Perfil(){
     dadosPaciente()
   },[])
 
+  function deslogar(){
+    AsyncStorage.removeItem('token')
+    AsyncStorage.removeItem('pacienteId')
+    navigation.replace('Login')
+  }
+
   return(
     <ScrollView flex={1}>
       <VStack flex={1} alignItems="center" p={5}>
         <Titulo color="blue.500">Meu Perfil</Titulo>
 
-        <Avatar size="xl" source={{ uri: "https://github.com/andreocunha.png" }} mt={5} />
+        <Avatar size="xl" source={{ uri: dadosPaciente?.imagem }} mt={5} />
 
         <Titulo color="blue.500">Informações pessoais</Titulo>
         <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
-        <Text>12/12/1990</Text>
-        <Text>São Paulo</Text>
+        <Text>{dadosPaciente?.email}</Text>
+        <Text>{dadosPaciente?.endereco?.estado}</Text>
 
         <Divider mt={5} />
 
-        <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
-        <Text>Bronquite</Text>
-        <Text>Sinusite</Text>
+        <Titulo color="blue.500" mb={1}>Planos de saúde</Titulo>
+        {
+          dadosPaciente?.planosSaude?.map((plano, index) => (
+            <Text key={index}>{plano}</Text>
+          ))
+        }
+
+        <Botao onPress={deslogar}>
+          Deslogar
+        </Botao>
       </VStack>
     </ScrollView>
   )
